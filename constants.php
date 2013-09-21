b2a2f43756a7cc02531112b0e96499ab88618e93
imgbb <drudmaster@gmail.com>
2013-09-22 01:26:13 +0300

    added PHPTAL, and part of the BASIC structure
    All of PHPTAL added, along with the current in-dev directories of the
    structure (will add the other directories on next commit, despite being
    empty). Will add Cake, back-end logic and temp front-end files soon.

---
+++ constants.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 9/19/13
+ * Time: 4:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+if (!defined(IBB_ROOT_PATH)) {
+    define('IBB_ROOT_PATH', dirname(__FILE__));
+}
+
+if (!defined(IBB_LIB_PATH))
+    define ('IBB_LIB_PATH', IBB_ROOT_PATH . '/lib');
+
+if (!defined(IBB_TEMPLATES_PATH))
+    DEFINE ('IBB_TEMPLATES_PATH', IBB_ROOT_PATH . '/TAL');
