702cd2011eb4e8a43c19cb5d675448eadc640dee
Truthlight <plainflower@yahoo.com>
2013-12-10 16:25:45 +0200

    added default

---
+++ app/user/modules/profile/defaultArea.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 12/7/13
+ * Time: 8:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+$AREA = 'view';
