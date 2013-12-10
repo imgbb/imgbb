56fe974580029041760f8048b5439420f5084025
Truthlight <plainflower@yahoo.com>
2013-12-10 16:21:42 +0200

    Nothing here

---
+++ app/user/modules/profile/view.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 12/7/13
+ * Time: 8:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class user_profile_view
+{
+	public $core;
+
+	public $db;
+
+	public function init()
+	{
+
+	}
+}
