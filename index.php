7db4b678da014a9335a01227cb956024891e8e70
Truthlight <plainflower@yahoo.com>
2014-01-06 19:54:10 +0200

    1 GS change.
    11-12:
    Added upload.php and text.php, this way of loading all these things is *completely and utterly temporary*
    
    39:
    NTS

--- index.php
+++ index.php
@@ -1,18 +1,25 @@
 <?php
-/* temp */
+
+require_once dirname(dirname(__FILE__)) . '/intruder_process.php';
+#temp
 $time_start = microtime(true);
 require_once 'constants.php';
 require_once IBB_ROOT_PATH . '/app/ibbController.php';
 require_once IBB_LIB_PATH . '/PHPTAL-1.2.2/PHPTAL.php';
 require_once IBB_ROOT_PATH . '/classes/furl_class.php';
 require_once IBB_ROOT_PATH . '/classes/loader_class.php';
-// TODO BASIC Exception handler
+require_once IBB_ROOT_PATH . '/classes/upload.php';
+require_once IBB_ROOT_PATH . '/classes/text/text.php';
 session_start();
-/* temp */
+//echo 'Loading all metadata takes ' . (microtime(true) - $time_start);
+//echo '<b>index</b><br />';
+//echo 'Exec: ' . (microtime(true) - $time_start) . '<br />';
+//echo 'Mem: '.turnMemoryToKB(memory_get_usage()) . '<br />';
 
-/* temporary catchall for exceptions until exceptions are prepped*/
+/* temporary catchall for exceptions until exceptions are prepped */
 try
 {
+
 	ibbController::run();
 } catch (Exception $e)
 {
@@ -24,15 +31,16 @@ try
 	}
 }
 
-// debugging & efficiency, temp
+# debugging & efficiency, temp
 $time_end = microtime(true);
 
 $execution_time = $time_end - $time_start;
 
+echo 'Execution time must not go above 1.0. Memory usage must not go above 5MB. Query exection count may not go above 15.<br />';
 echo '<b style="color:black;">Total Execution Time:</b> <span style="color:black;">'.$execution_time.'</span>';
 echo '<br /><b style="color:black;">Peak Memory Usage</b>: <span style="color:black;">'. turnMemoryToKB(memory_get_peak_usage()) . ' KB</span>';
-echo '<br /><b style="color:black;">Memory Usage at End of Script</b>: <span style="color:black;">' . turnMemoryToKB(memory_get_usage()) . ' KB</span>';
-echo '<br /><b style="color:black;">Query Execution Count</b>: <span style="color:black;">' . ibbCore::$queryc . '</span>';
+echo '<br /><b style="color:black;">Memory Usage at EoS</b>: <span style="color:black;">' . turnMemoryToKB(memory_get_usage()) . ' KB</span>';
+echo '<br /><b style="color:black;">Query Execution Count</b>: <span style="color:black;">' . count(ibbCore::$queryc) . '</span>';
 
 
 function turnMemoryToKB( $memory_get_usage )
