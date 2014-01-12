2f6cb2393341bc8a6db36964c5cb2118a5c3d40a
Truthlight <plainflower@yahoo.com>
2014-01-12 13:04:26 +0200

    1 GS change. 1 functionality feature. 1 bugfix
    16-18, 63-69:
    Cleanup; updated comments.
    
    33-45:
    Added escapeQuotes function mostly for interfaces that allow a user to interact with database rows.
    
    72:
    Fixed, there was a random dot in <a> elements.
    
    85-95:
    Made them all nice and aligned.

--- classes/text/text.php
+++ classes/text/text.php
@@ -13,19 +13,33 @@ class IBBText
 	public function __construct() {}
 
 	/**
-	 * @param null $options
+	 * @param string		$body
+	 *
+	 * @return string
 	 */
-	public static function PostParser($body, $options = null)
+	public static function PostParser($body)
 	{
 		#ew
 		$body = trim($body);
 		$body = htmlspecialchars($body, ENT_QUOTES);
 		$body = self::CreateLink($body);
 		$body = self::BBCode($body);
-		$body = str_replace('\\n', '<br />', $body);
 		$body = nl2br($body, TRUE);
 		$body = self::BuildPostLink($body);
 		return $body;
+	}
+
+	/**
+	 * @param string $text
+	 *
+	 * @return string
+	 */
+	public static function escapeQuotes( $text )
+	{
+		$text = str_replace("'", "\\'", $text);
+		$text = str_replace('"', '\\"', $text);
+
+		return $text;
 	}
 
 	/**
@@ -46,9 +60,16 @@ class IBBText
 
 	}
 
+	/**
+	 * Turn http:// URLs from plaintext to <a> elements.
+	 *
+	 * @param $text
+	 *
+	 * @return mixed
+	 */
 	public static function CreateLink($text)
 	{
-		$text =  preg_replace('#(?:http(s)?://)(w*?\.)?([\da-z-\.]+\.[a-z\.]{2,6})*(/[a-z/-_]*)#', '<a href=\"http\1://\2\3\4\">http\1://\2.\3\4</a>', $text);
+		$text =  preg_replace('#(?:http(s)?://)(w*?\.)?([\da-z-\.]+\.[a-z\.]{2,6})*(/[a-z/-_]*)#', '<a href=\"http\1://\2\3\4\">http\1://\2\3\4</a>', $text);
 		return $text;
 	}
 
@@ -61,17 +82,17 @@ class IBBText
 	 */
 	public static function BBCode( $text )
 	{
-		$bbcodes = array(	'[b]', '[/b]',
-			'[i]', '[/i]',
-			'[u]', '[/u]',
-			'[s]', '[/s]',
-			'[aa]', '[/aa]'
+		$bbcodes = array(	'[b]',	'[/b]',
+							'[i]',	'[/i]',
+							'[u]',	'[/u]',
+							'[s]',	'[/s]',
+							'[aa]',	'[/aa]'
 		);
-		$html = array(		'<span style=\"font-weight:bold;\">', 							'</span>',
-							'<span style=\"font-style:italic;\">', 							'</span>',
-							'<span style=\"border-bottom:1px solid\">', 						'</span>',
-							'<span style=\"text-decoration:line-through;\">', 				'</span>',
-							'<div style=\"font-family: Mona,\'MS PGothic\' !important;\">', 	'</div>'
+		$html = array(	'<span style=\"font-weight:bold;\">', 								'</span>',
+						'<span style=\"font-style:italic;\">',								'</span>',
+						'<span style=\"border-bottom:1px solid\">', 						'</span>',
+						'<span style=\"text-decoration:line-through;\">', 					'</span>',
+						'<div style=\"font-family: Mona,\'MS PGothic\' !important;\">', 	'</div>'
 
 		);
 		$text = str_replace($bbcodes, $html, $text);
