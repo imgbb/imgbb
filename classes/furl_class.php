81a61a929ca1834df667203996a9061c16061a2e
Truthlight <plainflower@yahoo.com>
2014-02-06 18:33:09 +0200

    1 GS change.
    6-88:
    Heavy modifications all around so that it can still work. It still needs a total rewrite though, but it's low priority atm.

--- classes/furl_class.php
+++ classes/furl_class.php
@@ -3,21 +3,38 @@
  * Class BasicFriendlyURL
  *
  * Retarded version of the friendly URL engine.
+ *
+ * TODO IMGBB rewrite
  */
 class BasicFriendlyURL {
 
 	private static $Templates = array(
 
-		'#/front/(\d+)-#' 	=> array( 'app' 	=>	'main',
-									  'mod'		=>	'front',
-									  'area'	=>	'view',
-									  'action'	=>	'$1'
+		array(
+			'app'		=> 'main',
+			'mod'		=> 'front',
+			'area'		=> 'view',
+			'furl'		=> '#/front/(\d*)-#',
+			'rfurl'		=> '/front/{$1}'
 		),
-		'#/(\w+)?/(\d+)?#'	=> array( 'app'		=>	'boards',
-									  'mod'		=>	'boardpage',
-									  'area'	=>	'view',
-									  'action'	=>	'1'
-		)
+		array(
+			'app'		=> 'boards',
+			'mod'		=> 'boardpage',
+			'area'		=> 'view',
+			'furl'		=> '#/(\w+)?/(\d+)?#',
+			'rfurl'		=> '/{$1}/{$2}'
+		),
+
+//		'#/front/(\d+)-#' 	=> array( 'app' 		=>	'main',
+//									  'mod'			=>	'front',
+//									  'area'		=>	'view',
+//									  'action'		=>	'$1'
+//		),
+//		'#/(\w+)?/(\d+)?#'	=> array( 'app'			=>	'boards',
+//									  'mod'			=>	'boardpage',
+//									  'area'		=>	'view',
+//									  'action'		=>	'$1',
+//		)
 	);
 
 	/**
@@ -25,21 +42,49 @@ class BasicFriendlyURL {
 	 *
 	 * @param $request string The request URL.
 	 */
-	public static function findpath($request)
+	public static function translateFURL( $request )
 	{
-		foreach (self::$Templates as $regex => $array)
+		foreach (self::$Templates as $array)
 		{
-			if (preg_match($regex, $request, $matches))
+			if (preg_match($array['furl'], $request, $matches))
 			{
-				$array['action'] = $matches[1];
+				$ret['action'] = $matches[1];
 				if (isset($matches[2]))
 				{
-					$array['subaction'] = $matches[2];
+					$ret['subaction'] = $matches[2];
 				}
-				ibbCore::$fURLArray = $array;
-				break;
+				$ret['app'] = $array['app'];
+				$ret['mod'] = $array['mod'];
+				$ret['area'] = $array['area'];
+				ibbCore::$fURLArray = $ret;
 			}
 		}
+		return FALSE;
 	}
+
+	/**
+	 *
+	 *
+	 * @param string $action
+	 * @param string $subaction
+	 * @param string $class
+	 *
+	 * @return string
+	 */
+	public static function toFURL( $action, $subaction, $class )
+	{
+		list($app, $mod, $area) = explode('_', $class);
+
+		foreach (self::$Templates as $array)
+		{
+			if ( $array['app'] == $app && $array['mod'] == $mod && $array['area'] == $area )
+			{
+				return str_replace(array('{$1}', '{$2}'), array($action, $subaction), $array['rfurl']);
+//				return preg_replace($array['rfurl'], $array['rfurl'], $array['furl']);
+			}
+		}
+		return FALSE;
+	}
+
 }
 
